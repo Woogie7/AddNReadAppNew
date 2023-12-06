36baf330bcf789e43025087bbb9aa18cd604989d
Kirill Tsyngeev <ktsyngeev@bk.ru>
2023-12-06 19:13:37 +0300

    asdas

--- AddNReadApp/Core/DBRegistrator.cs
+++ AddNReadApp/Core/DBRegistrator.cs
@@ -2,12 +2,13 @@
 using Microsoft.Extensions.DependencyInjection;
 
 
+
 namespace AddNReadApp.Core
 {
 	internal class DBRegistrator
 	{
-		//public static IServiceCollection AddDatabase(this IServiceCollection services, IConfiguration configuration) => services
-		//	.AddDbContext()
-		//;
+		public static IServiceCollection AddDatabase(this IServiceCollection services, IConfiguration configuration) => services
+			.AddDBContext();
+		;
 	}
 }
